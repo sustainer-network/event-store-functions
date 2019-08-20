ab1f5b48f1ea2d6af98bd68efb6b0a2a0208f77d
Joao <ritterjv@gmail.com>
2019-08-20 01:11:09 -0700

    -

--- test.js
+++ test.js
@@ -96,6 +96,67 @@ describe("Event store", () => {
       JSON.stringify({ a: "1", b: "2", c: "1" })
     );
   });
+  it("should get aggregates successfully when two events with the same timestamp are inserted", async () => {
+    const root = uuid();
+    const store = "some-store-id";
+    const service = "some-service";
+    const payload1 = {
+      a: 1,
+      b: 1
+    };
+    const payload2 = {
+      b: 2,
+      c: 1
+    };
+    await post(`${rootAddress}/add`, {
+      store,
+      service,
+      event: {
+        fact: {
+          root,
+          topic: "some-topic",
+          version: 0,
+          traceId: "a-trace-id",
+          command: "a.command",
+          commandInstanceId: "123",
+          commandIssuedTimestamp: 123,
+          createdTimestamp: 123
+        },
+        payload: payload1
+      }
+    });
+    const { body: aggregate0 } = await get(`${rootAddress}/aggregate`, {
+      store,
+      service,
+      root
+    });
+    expect(aggregate0).to.deep.equal(JSON.stringify({ a: "1", b: "1" }));
+
+    await post(`${rootAddress}/add`, {
+      store,
+      service,
+      event: {
+        fact: {
+          root,
+          topic: "some-topic",
+          version: 0,
+          traceId: "a-trace-id",
+          command: "a.command",
+          commandInstanceId: "123",
+          commandIssuedTimestamp: 123,
+          createdTimestamp: 123
+        },
+        payload: payload2
+      }
+    });
+
+    const { body: aggregate1 } = await get(`${rootAddress}/aggregate`, {
+      store,
+      service,
+      root
+    });
+    expect(aggregate1).to.deep.equal(JSON.stringify({ b: "2", c: "1" }));
+  });
   it("should return an error if aggregate has incorrect query params", async () => {
     const response = await get(`${rootAddress}/aggregate`, {});
     expect(response.statusCode).to.be.at.least(400);
