5d9e3cef7d3c01e1c7a7418029c69d2644ba807e
Joao <ritterjv@gmail.com>
2019-08-21 22:23:01 -0700

    -

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const eventStoreService = require("@sustainer-network/event-store-service");
-const tokenFromReq = require("@sustainer-network/token-from-req");
+const tokensFromReq = require("@sustainer-network/tokens-from-req");
 
 exports.add = (req, res) => {
   eventStoreService
-    .add({ body: req.body, token: tokenFromReq(req) })
+    .add({ body: req.body, tokens: tokensFromReq(req) })
     .then(() => res.send({}))
     .catch(e => res.status(e.statusCode).send(e));
 };
 
 exports.aggregate = (req, res) => {
   eventStoreService
-    .hydrate({ query: req.query, token: tokenFromReq(req) })
+    .hydrate({ query: req.query, token: tokensFromReq(req) })
     .then(aggregateRoot => {
       res.send(aggregateRoot);
     })
